209d327ae67d7ce9c8f54cdb8844623e57872860
hacker-kun <86187199+hacker-kun@users.noreply.github.com>
2022-12-05 10:01:31 -0800

    updated

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const io = new Server(server, {
   }
 });
 
-app.get("/", (req, res) => {res.send("Chat using Socket.io by Shwetank & Varun "); res.end()})
+app.get("/", (req, res) => {res.send("Chat using Socket.io by Shwetank"); res.end()})
 
 io.on("connection", (socket) => {
   console.log(socket.id)
